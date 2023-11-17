3766a3823097f320d611fdd210a136c9a9db20fb
AlexMari1234 <alex24marinescu@gmail.com>
2023-11-17 21:19:34 +0200

    Am modificat main

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,29 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
+
+
+class Engine {
+    private int pistons;
+    private String shape;
+
+    public Engine(int pistons, String shape) {
+        this.pistons = pistons;
+        this.shape = shape;
+    }
+
+    public void turnEngineOn() {
+        System.out.println("Engine started.");
+    }
+
+    public void turnEngineOff() {
+        System.out.println("Engine shut down.");
+    }
+}
+
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        System.out.println("ana are mere");
+        Engine engine = new Engine(5, "ana");
+        engine.turnEngineOff();
     }
 }
