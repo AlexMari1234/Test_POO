f01508f34a18cbc0894184f46e6b254dd299e660
AlexMari1234 <alex24marinescu@gmail.com>
2023-11-18 08:36:05 +0200

    Am adaugat Followers

--- src/sebi/Deiu.java
+++ src/sebi/Deiu.java
@@ -2,6 +2,7 @@ package sebi;
 
 public class Deiu {
     private int age;
+    private int followers;
 
     public int getAge() {
         return age;
@@ -11,7 +12,15 @@ public class Deiu {
         this.age = age;
     }
 
-    public void la_misto() {
-        System.out.println("ana are mere");
+    public void dada(int age) {
+        System.out.println(age + 5);
+    }
+
+    public void ana(int age) {
+        System.out.println(age + 4);
+    }
+
+    public int getFollowers() {
+        return followers;
     }
 }
